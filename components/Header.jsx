ee73289e9072ad25ef2fa2bb5d2c93e6dfdcbe28
Ksenia <baranova-ksenia1990@gmail.com>
2023-04-02 15:13:48 +0300

    add heart, basket, mobileNav

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,10 @@ import Wrapper from "./Wrapper";
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "./Menu";
+import { BsCart } from "react-icons/bs";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { VscChromeClose } from "react-icons/vsc";
+import { BiMenuAltRight } from "react-icons/bi";
 
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -24,6 +28,51 @@ const Header = () => {
           setShowCategMenu={setShowCategMenu}
           categories={categories}
         />
+
+        {/* heart, basket, mobileNav icon */}
+        <div className="flex items-center gap-2 text-black">
+          {/* heart icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              51
+            </div>
+          </div>
+          {/* icon end */}
+
+          {/* Basket icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <BsCart className="text-[15px] md:text-[20px]" />
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute 
+            top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px]
+            md:px-[5px]
+            "
+            >
+              5
+            </div>
+          </div>
+          {/* Icon end */}
+
+          {/* Mobile icon start */}
+          <div
+            className="md:hidden w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer
+          relative -mr-2
+          "
+          >
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px]"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px]"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+        </div>
       </Wrapper>
     </header>
   );
